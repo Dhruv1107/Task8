76f0e33c4877a38a3151b7eae2787c28ffe8358f
dhruv <dhruvsr4@gmail.com>
2019-08-30 07:37:00 +0530

    adding route guards

--- src/app/authenticate.service.ts
+++ src/app/authenticate.service.ts
@@ -1,45 +1,46 @@
-import { Injectable } from "@angular/core";
-import { Router } from "@angular/router";
-import { auth } from "firebase/app";
-import { AngularFireAuth } from "@angular/fire/auth";
-import { User } from "firebase";
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { auth } from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { User } from 'firebase';
 @Injectable({
-  providedIn: "root"
+	providedIn: 'root'
 })
 export class AuthenticateService {
-  user: User;
-  isAdmin: boolean;
-  constructor(public afAuth: AngularFireAuth, public router: Router) {
-    this.afAuth.authState.subscribe(user => {
-      if (user) {
-        this.user = user;
-        localStorage.setItem("user", JSON.stringify(this.user));
-      } else {
-        localStorage.setItem("user", null);
-      }
-    });
-  }
+	user: User;
+	isAdmin: boolean;
+	isLogin: boolean;
+	constructor(public afAuth: AngularFireAuth, public router: Router) {
+		this.afAuth.authState.subscribe((user) => {
+			if (user) {
+				this.user = user;
+				localStorage.setItem('user', JSON.stringify(this.user));
+			} else {
+				localStorage.setItem('user', null);
+			}
+		});
+	}
 
-  async login(email: string, password: string) {
-    try {
-      await this.afAuth.auth.signInWithEmailAndPassword(email, password);
-      email === "dhruv@gmail.com"
-        ? (this.isAdmin = true)
-        : (this.isAdmin = false);
-      this.router.navigate(["/ALL"]);
-    } catch (e) {
-      alert("Error!" + e.message);
-    }
-  }
+	async login(email: string, password: string) {
+		try {
+			await this.afAuth.auth.signInWithEmailAndPassword(email, password);
+			email === 'dhruv@gmail.com' ? (this.isAdmin = true) : (this.isAdmin = false);
+			this.isLogin = true;
+			this.router.navigate([ '/ALL' ]);
+		} catch (e) {
+			alert('Error!' + e.message);
+		}
+	}
 
-  async logout() {
-    await this.afAuth.auth.signOut();
-    localStorage.removeItem("user");
-    this.router.navigate(["/auth"]);
-  }
+	async logout() {
+		await this.afAuth.auth.signOut();
+		localStorage.removeItem('user');
+		this.isLogin = false;
+		this.router.navigate([ '/auth' ]);
+	}
 
-  get isLoggedIn(): boolean {
-    const user = JSON.parse(localStorage.getItem("user"));
-    return user !== null;
-  }
+	get isLoggedIn(): boolean {
+		const user = JSON.parse(localStorage.getItem('user'));
+		return user !== null;
+	}
 }
