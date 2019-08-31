8f8615facbd29c1b20adb9b9772f6b8eac94da08
dhruv <dhruvsr4@gmail.com>
2019-08-31 15:04:30 +0530

    Added route guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,22 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { AddformComponent } from "./addform/addform.component";
-import { BodyComponent } from "./body/body.component";
-import { PopupComponent } from "./popup/popup.component";
-import { AuthComponent } from "./auth/auth.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AddformComponent } from './addform/addform.component';
+import { BodyComponent } from './body/body.component';
+import { PopupComponent } from './popup/popup.component';
+import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth-guard.service';
 
 const routes: Routes = [
-  { path: "auth", component: AuthComponent },
-  { path: "addform", component: AddformComponent },
-  { path: "", redirectTo: "auth", pathMatch: "full" },
-  { path: "popup/:id", component: PopupComponent },
-  { path: ":name", component: BodyComponent }
+	{ path: 'auth', component: AuthComponent },
+	{ path: 'addform', component: AddformComponent, canActivate: [ AuthGuard ] },
+	{ path: '', redirectTo: 'auth', pathMatch: 'full' },
+	{ path: 'popup/:id', component: PopupComponent, canActivate: [ AuthGuard ] },
+	{ path: ':name', component: BodyComponent, canActivate: [ AuthGuard ] }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
-export const routingComponents = [
-  AddformComponent,
-  BodyComponent,
-  PopupComponent
-];
+export const routingComponents = [ AddformComponent, BodyComponent, PopupComponent ];
--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,34 +1,16 @@
-import { Injectable } from "@angular/core";
-import { AuthenticateService } from "./authenticate.service";
-import {
-  Router,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  CanActivate,
-  UrlTree
-} from "@angular/router";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { AuthenticateService } from './authenticate.service';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(
-    private authenticateService: AuthenticateService,
-    private router: Router
-  ) {}
+	constructor(private authenticateService: AuthenticateService, private router: Router) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> | Promise<boolean> | boolean {
-    return this.authenticateService.user.pipe(
-      map(user => {
-        const isAuth = !!user;
-        if (isAuth) {
-          return true;
-        }
-        return this.router.createUrlTree(["/auth"]);
-      })
-    );
-  }
+	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
+		console.log(this.authenticateService.isLogin);
+		if (this.authenticateService.isLogin) return true;
+		return this.router.createUrlTree([ '/auth' ]);
+	}
 }
